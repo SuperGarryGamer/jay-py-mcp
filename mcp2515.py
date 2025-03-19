e30cedfeec8b4eaa6cfa418fb352b939797414f7
SuperGarryGamer <melentyev.jay@gmail.com>
2025-03-19 12:50:54 +0100

    Implement queues, (de)serialization, interrupt handling. Should work, needs testing

--- mcp2515.py
+++ mcp2515.py
@@ -5,6 +5,8 @@ import queue
 
 class MCP2515:
     def __init__(self, interrupt_pin=25):
+        self.rx_queue = queue.Queue()
+        self.tx_queue = queue.Queue()
         self.spi = spidev.SpiDev()
         self.spi.open(0,0)
         self.spi.max_speed_hz = 10_000_000 # MCP2515 is rated for 10 MHz SPI
@@ -14,7 +16,7 @@ class MCP2515:
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(interrupt_pin, GPIO.IN)
         GPIO.add_event_detect(interrupt_pin, GPIO.FALLING, callback=self._on_interrupt, bouncetime=50)
-        self.set_register(0x2b, 0x03)
+        self.set_register(0x2b, 0x1f)
 
 
     def reset(self):
@@ -60,11 +62,35 @@ class MCP2515:
         
         return self.spi.xfer([0x03, address, 0x00])[2]
     
+    def get_status(self):
+        """Returns MCP2515 Interrupt status flags"""
+        return self.spi.xfer([0xa0, 0x00])[1]
+    
     def _on_interrupt(self):
         """Interrupt handler, do not call"""
+        flags = self.get_status()
+
+        if flags & 0b00000001 != 0: # RXB0 Full
+            raw_data = self.spi.xfer([0x90, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])[1:]
+            self.rx_queue.put(CAN_Frame(raw_data))
+        if flags & 0b00000010 != 0: # RXB1 Full
+            raw_data = self.spi.xfer([0x94, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])[1:]
+            self.rx_queue.put(CAN_Frame(raw_data))
+        if not self.tx_queue.empty():
+            if   flags & 0b00001000 != 0: # TXB0 Clear
+                self.set_registers(0x31, self.tx_queue.get().serialize())
+                self.spi.xfer([0x81])
+            elif flags & 0b00100000 != 0: # TXB1 Clear
+                self.set_registers(0x41, self.tx_queue.get().serialize())
+                self.spi.xfer([0x82])
+            elif flags & 0b10000000 != 0: # TXB2 Clear
+                self.set_registers(0x51, self.tx_queue.get().serialize())
+                self.spi.xfer([0x84])
+
         
 class CAN_Frame:  
     def __init__(self, extended: bool, remote: bool, id: int, data: list[int]):
+        """Initialize frame with given parameters"""
         if id > 0x1fffffff or (not extended and id > 0x7ff):
             raise OverflowError
         
@@ -77,5 +103,33 @@ class CAN_Frame:
         self.data = data
 
     def __init__(self, raw_data: list[int]):
-        pass
+        """Initialize frame from raw data from RXB0~1 registers"""
+        if raw_data[1] & 0b00001000 != 0:
+            self.extended = True
+        if raw_data[4] & 0b01000000 != 0:
+            self.remote = True
+
+        self.id = raw_data[0] << 3 + (raw_data[1] >> 5)
+        if self.extended:
+            self.id = (self.id << 18) + (raw_data[1] << 21) + (raw_data[2] << 8) + raw_data[3]
+        
+        self.data = []
+        for i in range(raw_data[4] & 0b00001111):
+            self.data.append(raw_data[5 + i])
+
+    def serialize(self):
+        """Returns sequence of bytes representing the frame that can be written directly to TXB0~2 registers"""
+        serialized = [0, 0, 0, 0, 0]
+        if self.extended:
+            serialized[0] = self.id >> 21
+            serialized[1] = ((self.id >> 18) & 0b111) << 5 + 8 + ((self.id >> 16) & 0b11) # + 8 sets the EXIDE bit
+            serialized[2] = (self.id >> 8) & 0xFF
+            serialized[3] = self.id & 0xFF
+            serialized[4] = len(self.data)
+            if self.remote:
+                serialized[4] += 64 # Set the RTR bit
+
+        serialized += self.data
+        return serialized
+
         
