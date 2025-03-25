b5cfca309e84278486716bf90d4af63d657bf6f6
SuperGarryGamer <melentyev.jay@gmail.com>
2025-03-25 23:15:38 +0100

    cleanuop should work?

--- mcp2515.py
+++ mcp2515.py
@@ -107,7 +107,7 @@ class MCP2515:
     def close(self):
         self.reset()
         self.spi.close()
-        GPIO.cleanup([8, 9, 10, 11, 25])
+        GPIO.cleanup()
     
     def _on_interrupt(self):
         """Interrupt handler, do not call"""
